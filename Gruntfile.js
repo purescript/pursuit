e31457f33113dfe577af3694525bf440879527b8
Phil Freeman <paf31@cantab.net>
2014-08-03 22:06:18 -0700

    Project setup

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({ 
+  
+    libFiles: [
+      "src/**/*.purs",
+      "bower_components/purescript-*/src/**/*.purs"
+    ],
+    
+    clean: ["tmp", "output"],
+  
+    pscMake: {
+      lib: {
+        src: ["<%=libFiles%>"]
+      }
+    },
+
+    dotPsci: ["<%=libFiles%>"]
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-purescript");
+ 
+  grunt.registerTask("make", ["pscMake:lib", "dotPsci"]);
+  grunt.registerTask("default", ["clean", "make"]);
+};
