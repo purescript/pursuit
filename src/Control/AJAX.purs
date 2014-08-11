d0672ea74147856ceaab1aa1d535ee3e63565591
Phil Freeman <paf31@cantab.net>
2014-08-10 20:06:46 -0700

    Prelude data, AJAX code

---
+++ src/Control/AJAX.purs
@@ -0,0 +1,23 @@
+module Control.AJAX where
+  
+import Control.Monad.Eff
+
+foreign import data XHR :: !
+
+type URI = String
+
+foreign import get 
+  "function get(uri) {\
+  \  return function(k) {\
+  \    return function() {\
+  \      var req = new XMLHttpRequest();\
+  \      req.onreadystatechange = function() {\
+  \        if (req.readyState === 4 && req.status === 200) {\
+  \          k(req.responseText);\
+  \        }\
+  \      };\
+  \      req.open('GET', uri, true);\
+  \      req.send();\
+  \    };\
+  \  };\
+  \}" :: forall eff. URI -> (String -> Eff (xhr :: XHR | eff) Unit) -> Eff (xhr :: XHR | eff) Unit 
--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,9 @@
 module Main where
 
+import Control.AJAX
+
 import qualified Data.Trie as T
 
-main = Debug.Trace.print $ 
-  Data.Trie.lookupAll "hel" $ 
-    Data.Trie.insert "hello" 10 $ 
-      Data.Trie.insert "hell" 9 $
-        Data.Trie.empty
+main = do
+  get "data.json" $ \json -> do
+    Debug.Trace.trace json
