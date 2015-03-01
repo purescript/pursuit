df0a5c2d61a79a380d6bf792704e3740235cc827
Harry Garrood <harry@garrood.me>
2015-03-01 09:18:13 +0000

    fix tabs/spaces

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -22,12 +22,12 @@ getDatabase = do
   (warns, _, eitherDb) <- generateDatabase testLibrariesFile
 
   if (null warns)
-	then putStrLn "Generated database. No warnings."
-	else mapM_ (putStrLn . show) warns
+    then putStrLn "Generated database. No warnings."
+    else mapM_ (putStrLn . show) warns
 
   case eitherDb of
-	Right db -> return db
-	Left err -> putStrLn (show err) >> exitFailure
+    Right db -> return db
+    Left err -> putStrLn (show err) >> exitFailure
 
 main :: IO ()
 main = do
@@ -35,57 +35,57 @@ main = do
   let query q = runQuery (queryDeclsJ q) db
   
   hspec $ do
-	describe "ADTs" $ do
-	  describe "with non-exported data constructors" $ do
-		specify "are not included in results" $ do
-		  query "ThisShouldNotBeExported" `shouldBe` []
-		  query "NeitherShouldThis" `shouldBe` []
+    describe "ADTs" $ do
+      describe "with non-exported data constructors" $ do
+        specify "are not included in results" $ do
+          query "ThisShouldNotBeExported" `shouldBe` []
+          query "NeitherShouldThis" `shouldBe` []
 
-	  describe "with exported data constructors" $ do
-		specify "are included in results" $ do
-		  query "ThisShouldBeExported" `shouldSatisfy` not . null
-		  query "SoShouldThis" `shouldSatisfy` not . null
+      describe "with exported data constructors" $ do
+        specify "are included in results" $ do
+          query "ThisShouldBeExported" `shouldSatisfy` not . null
+          query "SoShouldThis" `shouldSatisfy` not . null
 
-	describe "Values" $ do
-	  describe "which are exported" $ do
-		specify "should be included in results" $ do
-		  query "exportedFn" `shouldSatisfy` not . null
+    describe "Values" $ do
+      describe "which are exported" $ do
+        specify "should be included in results" $ do
+          query "exportedFn" `shouldSatisfy` not . null
 
-	  describe "which are not exported" $ do
-		specify "should not be included in results" $ do
-		  query "nonExportedFn" `shouldBe` []
+      describe "which are not exported" $ do
+        specify "should not be included in results" $ do
+          query "nonExportedFn" `shouldBe` []
 
-	  describe "without type signatures" $ do
-		specify "should still be included" $ do
-		  query "withoutTypeSig" `shouldSatisfy` not . null
+      describe "without type signatures" $ do
+        specify "should still be included" $ do
+          query "withoutTypeSig" `shouldSatisfy` not . null
 
-	  describe "with symbolic names" $ do
-		specify "should not require parenthesis" $ do
-		  let withParens = query "(<++>)"
-		  let noParens   = query "<++>"
+      describe "with symbolic names" $ do
+        specify "should not require parenthesis" $ do
+          let withParens = query "(<++>)"
+          let noParens   = query "<++>"
 
-		  withParens `shouldSatisfy` not . null
-		  withParens `shouldBe` noParens
+          withParens `shouldSatisfy` not . null
+          withParens `shouldBe` noParens
 
-	describe "Typeclass members" $ do
-	  specify "should include the correct constraints in their types" $ do
-		let result = map (\(d, _, _) -> d) (query "thingy")
-		let thingy' = filter ((== DeclName "thingy") . declName) result
+    describe "Typeclass members" $ do
+      specify "should include the correct constraints in their types" $ do
+        let result = map (\(d, _, _) -> d) (query "thingy")
+        let thingy' = filter ((== DeclName "thingy") . declName) result
 
-		thingy <- exactlyOne thingy'
-		runDeclDetail (declDetail thingy) `shouldHaveSubstring` "(Thingy a) =>"
-	
-	describe "Primitive types" $ do
-	  describe "should appear in the results" $ do
-		forM_ ["Function", "String", "Number", "Array", "Object", "Boolean"] $ \prim -> do
-		  specify (T.unpack prim) $
-			query prim `shouldSatisfy` not . null
+        thingy <- exactlyOne thingy'
+        runDeclDetail (declDetail thingy) `shouldHaveSubstring` "(Thingy a) =>"
+    
+    describe "Primitive types" $ do
+      describe "should appear in the results" $ do
+        forM_ ["Function", "String", "Number", "Array", "Object", "Boolean"] $ \prim -> do
+          specify (T.unpack prim) $
+            query prim `shouldSatisfy` not . null
 
   where
   exactlyOne :: [a] -> IO a
   exactlyOne [x] = return x
   exactlyOne _ = expectationFailure "expected a list with exactly one element"
-				  >> return undefined
+                  >> return undefined
 
   shouldHaveSubstring str substr =
-	str `shouldSatisfy` (not . null) . TL.breakOnAll substr
+    str `shouldSatisfy` (not . null) . TL.breakOnAll substr
