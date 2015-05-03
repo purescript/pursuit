70eed3d2bbbfb7b6a834d36dc90ce5c33d423a7e
Harry Garrood <harry@garrood.me>
2015-05-03 11:58:14 +0100

    Add styles from the mockup

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -60,6 +60,8 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
+            addStylesheet $ StaticR css_normalize_css
+            addStylesheet $ StaticR css_style_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
