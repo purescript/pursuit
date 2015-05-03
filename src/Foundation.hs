eb53f7d77ec5e0daa4b34c5aef428968a01e33a7
Harry Garrood <harry@garrood.me>
2015-05-03 11:54:22 +0100

    Get rid of bootstrap

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -60,7 +60,6 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
-            addStylesheet $ StaticR css_bootstrap_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
