6c50554005d04d813b95d3a512b48341550bda17
Harry Garrood <harry@garrood.me>
2015-05-13 03:38:57 +0100

    Rounded corners for badges

--- src/Handler/PackageBadges.hs
+++ src/Handler/PackageBadges.hs
@@ -24,12 +24,17 @@ getPackageBadgeR (PathPackageName pkgName) = do
 renderBadge :: Version -> S.Svg
 renderBadge version =
   S.docTypeSvg ! A.version "1.1" ! A.width (i totalWidth) ! A.height (i totalHeight) $ do
-    S.g $ do
-      S.path ! A.fill "#555" ! A.d leftPath
-      S.path ! A.fill "#e25" ! A.d rightPath
-    S.g ! A.fill "#fff" ! A.textAnchor "middle" ! A.fontSize "12" ! A.fontFamily "sans-serif" $ do
-      leftText   "pursuit"
-      rightText  ('v' : showVersion version)
+    S.defs $ do
+      S.clippath ! A.id_ "clipPath" $
+        S.rect ! A.x (i 0) ! A.y (i 0) ! A.width (i totalWidth) ! A.height (i totalHeight)
+               ! A.rx (i cornerRadius) ! A.ry (i cornerRadius)
+    S.g ! A.style "clip-path: url(#clipPath)" $ do
+      S.g $ do
+        S.path ! A.fill "#555" ! A.d leftPath
+        S.path ! A.fill "#e25" ! A.d rightPath
+      S.g ! A.fill "#fff" ! A.textAnchor "middle" ! A.fontSize "12" ! A.fontFamily "sans-serif" $ do
+        leftText   "pursuit"
+        rightText  ('v' : showVersion version)
   where
   i' :: Int -> String
   i' = show
@@ -40,6 +45,7 @@ renderBadge version =
   totalHeight = 20
   partitionX = 50
   partitionY = totalWidth - partitionX
+  cornerRadius = 3
 
   leftPath = toValue $
     concat ["M0 0 h", i' partitionX, " v", i' totalHeight, " H0 z"]
