39b91b169b93087c9d79e661947a46bc8a8696ad
Harry Garrood <harry@garrood.me>
2015-07-04 00:07:55 +0100

    Very basic Hoogle integration

--- src/Handler/Hoogle.hs
+++ src/Handler/Hoogle.hs
@@ -2,16 +2,100 @@
 module Handler.Hoogle where
 
 import Import
+import Control.Category ((>>>))
 import qualified Data.Text.Lazy as LT
 import qualified Hoogle
+import qualified Language.PureScript.Docs as D
+import qualified Web.Bower.PackageMeta as Bower
 
 import Model.DocsAsHoogle (packageAsHoogle)
-import Handler.Packages (findPackage)
+import Handler.Database
+import Handler.Packages (findPackage, getLatestVersion)
 import Handler.Caching (cacheText)
+import Handler.Utils
 
 getPackageHoogleR :: PathPackageName -> PathVersion -> Handler LT.Text
 getPackageHoogleR (PathPackageName pkgName) (PathVersion version) = 
   cacheText $ findPackage pkgName version (return . packageAsHoogle)
 
 generateDatabase :: Handler Hoogle.Database
-generateDatabase = undefined
+generateDatabase = do
+  packages <- getAllPackages
+  let inputs = map (unpack . packageAsHoogle) packages
+  let inputData = concat $ intersperse "\n\n" $ inputs
+  outputFile <- (++) <$> getWorkingDirectory <*> getTimestampedFilename "data.tmp.hoo"
+
+  -- Just to ensure that parent directories exist
+  writeFileWithParents outputFile ("<test>" :: Text)
+
+  (db, errs) <- liftIO $ createDatabase inputData outputFile
+  -- TODO: traverse warn errs
+  return db
+
+-- | Gets the directory used as a working directory for database generation.
+getWorkingDirectory :: Handler FilePath
+getWorkingDirectory = (++ "/hoogle/work/") <$> getDataDir
+
+-- | Get a temporary file name with the given suffix. Eg:
+-- "test.txt" -> "20150628.test.txt"
+getTimestampedFilename :: (MonadIO m) => String -> m String
+getTimestampedFilename suffix = do
+  time <- liftIO getCurrentTime
+  return $ formatTime defaultTimeLocale "%d%m%y%H%M%S." time ++ suffix
+
+dummyHackageUrl :: String
+dummyHackageUrl = "dummy.hackage.url/"
+
+-- | Given a Hoogle result url, try to extract the name of the package and the
+-- title of the relevant declaration.
+extractPackageAndTitle :: String -> Maybe (Bower.PackageName, String)
+extractPackageAndTitle url = (,) <$> extractPackage url <*> pure (extractTitle url)
+  where
+  extractPackage =
+    stripPrefix (dummyHackageUrl ++ "package/")
+    >>> map (takeWhile (/= '/'))
+    >=> (rightMay . Bower.parsePackageName)
+
+  extractTitle =
+    reverse
+    >>> takeWhile (/= ':')
+    >>> reverse
+    -- TODO: decode
+
+  rightMay (Right x) = Just x
+  rightMay _         = Nothing
+
+searchDatabase :: Hoogle.Database -> String -> Handler [(Bower.PackageName, String)]
+searchDatabase db query =
+  case Hoogle.parseQuery Hoogle.Haskell query of
+    Left err ->
+      fail $ show err
+    Right q ->
+      let results = Hoogle.search db q
+      in  return $ mapMaybe munge results
+  where
+  munge =
+    extractPackageAndTitle <=< resultUrl . snd
+
+  resultUrl r =
+    case Hoogle.locations r of
+      [(x, _)] -> Just x
+      _        -> Nothing
+
+createDatabase ::
+  String -- ^ Hoogle input data
+  -> FilePath -- ^ Output file name
+  -> IO (Hoogle.Database, [Hoogle.ParseError])
+createDatabase inputData outputFile = do
+  errs <- Hoogle.createDatabase dummyHackageUrl Hoogle.Haskell [] inputData outputFile
+  db <- Hoogle.loadDatabase outputFile
+  return (db, errs)
+
+-- | This is horribly inefficient, but it will do for now.
+getAllPackages :: Handler [D.VerifiedPackage]
+getAllPackages = do
+  pkgNames <- getAllPackageNames
+  pkgNamesAndVersions <- catMaybes <$> traverse withVersion pkgNames
+  catMaybes <$> traverse (uncurry lookupPackage) pkgNamesAndVersions
+  where
+  withVersion name = (map . map) (name,) (getLatestVersion name)
