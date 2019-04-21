98fbd2d8d8eb90dcc194b35cce0a5f679a142b72
Harry Garrood <harry@garrood.me>
2019-04-21 23:14:34 +0100

    Speed up travis
    - pass --fast on non-tagged builds
    - simplify config

--- travis/script.sh
+++ travis/script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 stack setup
 stack --version
-stack install alex happy
-stack --no-terminal --jobs=1 build --only-dependencies
-stack --no-terminal --jobs=1 test --flag pursuit:-dev --pedantic
+
+if [ -n "$TRAVIS_TAG" ]
+then
+  STACK_EXTRA_FLAGS="--flag pursuit:-dev"
+else
+  STACK_EXTRA_FLAGS="--fast"
+fi
+
+stack --no-terminal --jobs=2 build --only-dependencies $STACK_EXTRA_FLAGS
+stack --no-terminal --jobs=2 test --pedantic $STACK_EXTRA_FLAGS
