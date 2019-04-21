cb4272f666215add7cd162c7ffda4d7c889214cc
Harry Garrood <harry@garrood.me>
2019-04-21 23:47:40 +0100

    Build with --fast on non-release builds

--- travis/script.sh
+++ travis/script.sh
@@ -6,12 +6,16 @@ curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip
 stack setup
 stack --version
 
-if [ -n "$TRAVIS_TAG" ]
+if [ -z "$TRAVIS_TAG" ]
 then
-  STACK_EXTRA_FLAGS="--flag pursuit:-dev"
-else
   STACK_EXTRA_FLAGS="--fast"
 fi
 
-stack --no-terminal --jobs=2 build --only-dependencies $STACK_EXTRA_FLAGS
-stack --no-terminal --jobs=2 test --pedantic $STACK_EXTRA_FLAGS
+stack --no-terminal --jobs=2 build \
+  --flag pursuit:-dev \
+  --only-dependencies \
+  $STACK_EXTRA_FLAGS
+stack --no-terminal --jobs=2 test \
+  --flag pursuit:-dev \
+  --pedantic \
+  $STACK_EXTRA_FLAGS
